435706001fe3ad4793eb05bfefc9fa6baf82eb3f
Jose Mendoza <43755829+hndrjose@users.noreply.github.com>
2020-03-21 11:22:18 -0600

    diecisies upload Front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ const hostname = hostnameArray[1].replace('//', '');
 
 export const environment = {
   production: true,
-  url: `https://crucial-accord-261801.appspot.com/`
+  url: `https://crucial-accord-261801.appspot.com`
 };
